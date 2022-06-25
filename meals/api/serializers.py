0e16f59749af9a2ce9dcec070dd5e54def258e88
OliverBiel <gmartins.oliver@gmail.com>
2022-06-25 12:14:33 -0300

    feat: Show foods data

--- meals/api/serializers.py
+++ meals/api/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from servings.api.serializers import ServingSerializer
 from meals.models import Meal
 
 
 class MealSerializer(ModelSerializer):
+    foods = ServingSerializer(many=True)
+
     class Meta:
         model = Meal
         fields = ['name', "foods"]
--- meals/models.py
+++ meals/models.py
@@ -6,10 +6,6 @@ class Meal(models.Model):
     name = models.CharField(max_length=250)
     foods = models.ManyToManyField(Serving)
 
-    def ShowFoodsName(self):
-        foods = [foods.id for foods in self.foods.all()]
-        return Serving.objects.filter()
-
     def __str__(self):
         return self.name
         
