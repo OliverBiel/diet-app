5f7eec7df5dc42cc4ad4ec6124abb9ecc0c6d17e
OliverBiel <gmartins.oliver@gmail.com>
2022-07-20 17:12:50 -0300

    feat: name in frontend - foods

--- frontend/dietapp/src/app/meals/meals.component.ts
+++ frontend/dietapp/src/app/meals/meals.component.ts
@@ -12,26 +12,37 @@ export class MealsComponent implements OnInit {
 
   meals!: Meal[];
   foods!: Food[];
+  foodsNames: {[id: number]: string} = {};
 
   constructor( private api: MealsServiceService ) { 
    }
 
-  ngOnInit(): void {
-    this.getMeals();
-    this.getFoods();
+  async ngOnInit(): Promise<void> {
+    await this.getFoods();
+    // await this.getMeals();
+    console.log(this.meals);
+
   }
   
   getMeals(){
     this.api.getMeals().subscribe(data => {
       this.meals = data;
+      for(let meal of this.meals) {
+        for(let food of meal.foods) {
+         food.foods = this.foodsNames[food.foods];
+        }
+      }
     });
   };
 
   getFoods(){
     this.api.getFood().subscribe(data => {
       this.foods = data;
+      for (let food of this.foods) {
+        this.foodsNames[food.id] = food.name;
+      }
+      this.getMeals();
     });
-    return this.foods;
   };
 
 }
